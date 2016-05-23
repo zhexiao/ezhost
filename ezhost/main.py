e872204aa18186d34ce0754491b5581794d9107f
zhexiao <zhexiao@163.com>
2016-05-23 10:44:14 -0400

    add

--- ezhost/main.py
+++ ezhost/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 This module contains `main` method plus related subroutines.
 `main` is executed as the command line ``ezhost`` program and takes care of
@@ -7,5 +8,24 @@ The other callables defined in this module are internal only. Anything useful
 to individuals leveraging Fabric as a library, should be kept elsewhere.
 """
 
+import sys
+
+from ServerLists import ServerLists
+from ServerLamp import ServerLamp
+
 def main():
-    print('there is main entry')
+
+    """
+    Check args
+    """
+    try:
+        arg1 = sys.argv[0]
+        arg2 = sys.argv[1]
+        server = ServerLists(arg2)
+    except Exception as e:
+        raise e
+
+    """
+    Call server install function
+    """
+    eval(server.name)().install()
