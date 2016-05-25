ed8a33afcc57d4bf64e0a562017c5242c9456fd3
zhexiao <zhexiao@163.com>
2016-05-25 09:20:58 -0400

    version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.1.5',
+    version='1.2.5',
 
     description='Make life easy to install server',
     long_description=long_description,
