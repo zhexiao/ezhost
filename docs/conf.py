304a43e6ace5f3046fd005eca3a52f0ca8d08cd1
zhexiao <zhexiao@163.com>
2016-05-27 00:49:10 -0400

    change latex_show_pagerefs

--- docs/conf.py
+++ docs/conf.py
@@ -240,7 +240,7 @@ latex_documents = [
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-latex_show_pagerefs = True
+#latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
