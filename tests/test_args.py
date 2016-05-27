714f62f97cccc2cdacb5c25fa34abdcb20ec15a2
zhexiao <zhexiao@163.com>
2016-05-27 01:12:41 -0400

    add tests for configparse

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,6 +1,7 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+"""
+This test is for command line args 
+"""
+
 import unittest
 import argparse
 
