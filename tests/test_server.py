e2f213a7bce005fb0b0826cffd9956a1955c86eb
zhexiao <zhexiao@163.com>
2017-07-21 09:02:24 +0800

    remove test

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,27 +3,6 @@
 """
 
 import unittest
-import argparse
-import configparser
-from ezhost.BigDataArchi import BigDataArchi
-
-
-class ServerTest(unittest.TestCase):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-s', '--server',
-        help='服务器代替名',
-    )
-
-    def test_bigdata(self):
-        """
-        Test ssh generate
-        :return:
-        """
-        args = self.parser.parse_args([
-            '-'
-        ])
-        BigDataArchi(args).install()
 
 if __name__ == '__main__':
     unittest.main()
